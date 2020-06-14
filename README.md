df449dff4c3ca0a715417eecc5f9a25f472655c6
Brian Ingles <github@emeraldwalk.com>
2020-06-13 21:04:37 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Server
+Project was created with AWS cdk
+https://docs.aws.amazon.com/cdk/api/latest/
+
+Project was initiated with
+
+```
+cdk init --language typescript
+```
---
+++ server/bin/server.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { ServerStack } from '../lib/server-stack';
+
+const app = new cdk.App();
+new ServerStack(app, 'ServerStack');
